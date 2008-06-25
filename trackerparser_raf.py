952ace09d05fb69d9c25ad86ab0dc3554f6c9bfa
Saverio Proto <zioproto@gmail.com>
2008-06-25 13:41:58 +0000

    This fixes a bug about notation of the tracker that does not use seconds but only degree and minutes
    See documentation of the protocol at the last page for details
    
    
    
    git-svn-id: http://svn.ninux.org/svn/ninuxdeveloping/Tracker2GMaps@548 ef3c9aa7-e928-0410-9a28-8491e62b674b

--- trackerparser_raf.py
+++ trackerparser_raf.py
@@ -22,28 +22,28 @@ class EchoRequestHandler(SocketServer.BaseRequestHandler ):
 		data = data[inizio:fine]
 		LatiG = data[0:2]
 		print 'LatiG: ' + LatiG
-		LatiM = data[2:4]
+		LatiM = data[2:9]
 		print 'LatiM: ' + LatiM
-		LatiS = data[5:9]
-		LatiS = LatiS[:2] + "."+ LatiS[2:5]
-		print 'LatiS: ' + LatiS
+		#LatiS = data[5:9]
+		#LatiS = LatiS[:2] + "."+ LatiS[2:5]
+		#print 'LatiS: ' + LatiS
 		
 
 		LongG = data[13:15]
 		print 'LongG: ' + LongG
-		LongM = data[15:17]
+		LongM = data[15:22]
 		print 'LongM: ' + LongM
-		LongS = data[18:22]
-		LongS = LongS[:2] + "."+ LongS[2:5]
-		print 'LongS: ' + LongS
+		#LongS = data[18:22]
+		#LongS = LongS[:2] + "."+ LongS[2:5]
+		#print 'LongS: ' + LongS
 
 		# Input coordinate in DMS format
 		#coord = { "degrees": 121, "minutes": 8, "seconds": 6 }
 
 		# Convert coordinate from DMS to DD
-		Latitudine = dd.dms2decimal(LatiG, LatiM, LatiS)
+		Latitudine = dd.dms2decimal(LatiG, LatiM, 0)
 		print Latitudine	
-		Longitudine = dd.dms2decimal(LongG, LongM, LongS)
+		Longitudine = dd.dms2decimal(LongG, LongM, 0)
 		print Longitudine	
 		conn = MySQLdb.connect(host="localhost",
 		user="tracker",
